4884d2da9880a849347cf96c7f948a5bb74a9f1b
sean <seanowenhayes@gmail.com>
2015-12-12 19:04:11 +0100

    Added some more tests.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-module.exports = function() {
+
+module.exports = function(options) {
     return function(page, spider, next) {
+        page.data = page.data || {};
+        Object.keys(options).forEach(function(key) {
+            page.data[key] = page.$(options[key]).map(function() {
+                return page.$(this).attr('src');
+            });
+        });
         next();
     }
-}
+};
--- test/index.js
+++ test/index.js
@@ -1,17 +1,44 @@
 var cssImageExtraction = require('..');
+var chai = require('chai');
+var should = chai.should();
 
 describe('mrspider-css-image-extraction', function() {
 
     var validPage;
     var validSpider;
+    var validNext;
+    var validOptions;
 
     beforeEach(function() {
         validPage = {};
         validSpider = {};
+        validNext = function() {};
+        validOptions = {};
     });
 
     it('should call the next argument', function(done) {
-        var imageExtraction = cssImageExtraction();
+        var imageExtraction = cssImageExtraction(validOptions);
         imageExtraction(validPage, validSpider, done);
+    });
+
+    it('should create a data property on the page', function() {
+        var imageExtraction = cssImageExtraction(validOptions);
+        imageExtraction(validPage, validSpider, validNext);
+        should.exist(validPage.data);
+    });
+
+    it('should not overwrite an existing data property on the page', function() {
+        var imageExtraction = cssImageExtraction(validOptions);
+        validPage.data = {msg: 'hi'};
+        imageExtraction(validPage, validSpider, validNext);
+        validPage.data.msg.should.equal('hi');
+    });
+
+    it('should extract images', function() {
+        validPage.content = `
+            <div> <a href="main.jpeg" class="main"></a><a href="thumb1.jpg" class="thumb"></a><a href="thumb2.jpg" class="thumb"></a><a href="thumb3.jpg" class="thumb"></a></div>
+        `;
+        var imageExtraction = cssImageExtraction(validOptions);
+
     });
 });
