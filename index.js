bd45c11490ef093ae2030efa340676bb391b1400
VERMICULITE <seanowenhayes@gmail.com>
2016-03-01 21:30:40 +0000

    Made it an bject stream

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let through2 = require('through2');
 
 module.exports = function (options, alternateSrcAttribute) {
     var srcAttribute = alternateSrcAttribute || 'src';
-    return through2(function (page, enc, next) {
+    return through2.obj(function (page, enc, next) {
         page.data = page.data || {};
         Object.keys(options).forEach(function (key) {
             page.$(options[key]).each(function () {
